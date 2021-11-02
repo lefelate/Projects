29ea1953894807c8ece80f1f8c3b402e46388844
bla <mawusiraphael@gmail.com>
2021-11-02 22:47:20 +0000

    second commit

--- Andy/AndyWork1/script.js
+++ Andy/AndyWork1/script.js
@@ -1,5 +1,5 @@
 const countEl=document.getElementById('count_el');
-
+const headingEl=document.getElementById('heading')
 let count=0;
 
 let increment =setInterval(() => {
@@ -8,6 +8,7 @@ let increment =setInterval(() => {
     if(count===10){
         count=10;
         clearInterval(increment);
+        headingEl.innerText="Decreasing numbers";
         let decrement=setInterval(() => {
             count--;
            countEl.innerText=count;
