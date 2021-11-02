7db4a513b58406bfd8225179299f50f1d94e5a78
bla <mawusiraphael@gmail.com>
2021-11-02 21:53:26 +0000

    decrement added to the increment

---
+++ Andy/AndyWork1/script.js
@@ -0,0 +1,22 @@
+const countEl=document.getElementById('count_el');
+
+let count=0;
+let increment =setInterval(() => {
+    count= count+1;
+    countEl.innerText=count;
+    if(count===10){
+        count=10;
+        clearInterval(increment);
+        let decrement=setInterval(() => {
+            count--;
+           countEl.innerText=count;
+           if( count==0){
+               clearInterval(decrement);
+           }
+       }, 1000);
+    }
+    return count;
+}, 1000);
+
+
+
